1c040a74c90a7419f2c881d008a531a9624476e1
Oleksandr Komendant <avkomq@gmail.com>
2018-05-13 23:32:52 +0300

    Add ErrorBoundary component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import ErrorBoundary from './ErrorBoundary';
 
 export default class App extends React.Component {
     render() {
-        return <h1>Hello, React!</h1>;
+        return (
+            <ErrorBoundary>
+                <h1>Hello, React!</h1>
+            </ErrorBoundary>);
     }
 }
