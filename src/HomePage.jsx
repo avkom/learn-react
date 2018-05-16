c348dffb6cbd26b894e60ce186e9d358a9adea47
Oleksandr Komendant <avkomq@gmail.com>
2018-05-16 16:17:34 +0300

    Add SearchHeader.

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import SearchHeader from './SearchHeader';
 
 export default class HomePage extends React.Component {
     render() {
         return (
             <div>
                 <Header />
+                <SearchHeader />
                 <Footer />
             </div>
         );
---
+++ src/SearchHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class SearchHeader extends React.Component {
+    render() {
+        return (
+            <div>Find your moovie</div>
+        );
+    }
+}
