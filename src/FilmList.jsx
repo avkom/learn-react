1e5dd75fd7a2c1db866d1a9dd9accd87892e6afe
Oleksandr Komendant <avkomq@gmail.com>
2018-05-17 00:02:19 +0300

    Fix year format, show "No films found"

--- src/FilmList.jsx
+++ src/FilmList.jsx
@@ -6,11 +6,16 @@ export default class FilmList extends React.Component {
         return (
             <div>
                 {
-                    this.props.items.map(
-                        (item) => {
-                            return <FilmMiniCard key={item.id} {...item} />
-                        }
-                    )
+                    this.props.items.length > 0
+                        ? this.props.items.map(
+                            (item) => {
+                                return <FilmMiniCard key={item.id} {...item} />
+                            }
+                        )
+
+                        : (
+                            <div>No films found</div>
+                        )
                 }
             </div>
         );
--- src/FilmMiniCard.jsx
+++ src/FilmMiniCard.jsx
@@ -5,10 +5,8 @@ export default class FilmMiniCard extends React.Component {
         return (
             <div>
                 <img src={this.props.poster_path} alt={this.props.title} />
-                <div>
-                    <span>{this.props.title}</span>
-                    <span>{this.props.release_date}</span>
-                </div>
+                <div>{this.props.title}</div>
+                <div>{new Date(this.props.release_date).getFullYear()}</div>
                 <div>{this.props.genres.join(', ')}</div>
             </div>
         );
