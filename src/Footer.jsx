f70a50feff574e04f313fe7b6df19fdeb7aa31ab
Oleksandr Komendant <avkomq@gmail.com>
2018-05-16 16:04:23 +0300

    Add Footer.

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from './Logo'
+
+export default class Footer extends React.Component {
+    render() {
+        return (
+            <header>
+                <Logo />
+            </header>
+        );
+    }
+}
--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
 
 export default class HomePage extends React.Component {
     render() {
         return (
-            <Header />
+            <div>
+                <Header />
+                <Footer />
+            </div>
         );
     }
 }
