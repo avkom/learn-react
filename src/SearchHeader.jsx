1df0dcf51f378c3b650456ccc232a94b020f4f5b
Oleksandr Komendant <avkomq@gmail.com>
2018-05-16 19:33:28 +0300

    Implemented Toggle component.

--- src/SearchHeader.jsx
+++ src/SearchHeader.jsx
@@ -2,18 +2,43 @@ import React from 'react';
 import Toggle from './Toggle';
 
 export default class SearchHeader extends React.Component {
+    searchByItems = [
+        {value: 'title', label: 'Title'},
+        {value: 'genres', label:'Genre'}
+    ];
+
+    sortByItems = [
+        {value: 'release_date', label: 'release date'},
+        {value: 'vote_average', label:'rating'}
+    ];
+    
     constructor(props) {
         super(props);
-        this.state = {term: ''};
+
+        this.state = {
+            term: '',
+            searchBy: 'title',
+            sortBy: 'vote_average'
+        };
     }
 
     handleTermChange = (event) => {
         this.setState({term: event.target.value});
     }
     
+    handleSearchByChange = (value) => {
+        this.setState({searchBy: value});
+    }
+
+    handleSortByChange = (value) => {
+        this.setState({sortBy: value});
+    }
+
     handleSearchClick = () => {
         let searchRequest = {
-            search: this.state.term
+            search: this.state.term,
+            searchBy: this.state.searchBy,
+            sortBy: this.state.sortBy
         };
 
         this.props.onSearch(searchRequest);
@@ -26,14 +51,14 @@ export default class SearchHeader extends React.Component {
                 <input type='text' value={this.state.term} onChange={this.handleTermChange} />
                 <div>
                     <span>Search by</span>
-                    <Toggle />
+                    <Toggle value={this.state.searchBy} items={this.searchByItems} onChange={this.handleSearchByChange} />
                     <button type='button' onClick={this.handleSearchClick}>Search</button>
                 </div>
                 <div>
-                    <span>7 movies found</span>
+                    <span>{this.props.resultCount} movies found</span>
                     <div>
                         <span>Sort by</span>
-                        <Toggle />
+                        <Toggle value={this.state.sortBy} items={this.sortByItems} onChange={this.handleSortByChange} />
                     </div>
                 </div>
             </div>
--- src/Toggle.jsx
+++ src/Toggle.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 
 export default class Toggle extends React.Component {
+    handleChange = (event) => {
+        this.props.onChange(event.target.value);
+    }
+
     render() {
         return (
             <div>
-                <input type='radio' />Title
-                <input type='radio' />Genre
+                {
+                    this.props.items.map(
+                        (item) => {
+                            return (
+                                <label>
+                                    <input 
+                                        type='radio' 
+                                        value={item.value} 
+                                        checked={item.value === this.props.value}
+                                        onChange={this.handleChange}
+                                    />
+                                    {item.label}
+                                </label>
+                            )
+                        }
+                    )
+                }
             </div>
         );
     }
