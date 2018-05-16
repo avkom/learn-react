360bd3ee9e6d2d17dc7adbd667d1aadee3a6cce1
Oleksandr Komendant <avkomq@gmail.com>
2018-05-17 01:07:51 +0300

    Add rating and description to film details page.

--- src/FilmDetails.jsx
+++ src/FilmDetails.jsx
@@ -6,7 +6,9 @@ export default class FilmDetails extends React.Component {
             <div>
                 <img src={this.props.poster_path} alt={this.props.title} />
                 <div>{this.props.title}</div>
+                <div>{this.props.vote_average}</div>
                 <div>{new Date(this.props.release_date).getFullYear()}</div>
+                <div>{this.props.overview}</div>
                 <div>{this.props.genres.join(', ')}</div>
             </div>
         );
