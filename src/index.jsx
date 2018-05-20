20cda21e03a0416a89ad9775772ff0d408607c55
Oleksandr Komendant <avkomq@gmail.com>
2018-05-20 23:31:52 +0300

    Add redux-persist

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,26 @@ import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-import filmsApp from './reducers';
+import {persistStore, persistReducer} from 'redux-persist';
+import {PersistGate} from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+import rootReducer from './reducers';
 import App from './components/App';
 
-const store = createStore(filmsApp, applyMiddleware(thunk));
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = createStore(persistedReducer, applyMiddleware(thunk));
+const persistor = persistStore(store);
 
 render(
-    <Provider store={store} >
-        <App />
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>,
     document.getElementById('app')
 );
