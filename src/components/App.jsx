6c01eb6a4621fd2232985a1ebdd2b57493898620
Oleksandr Komendant <avkomq@gmail.com>
2018-05-20 22:42:47 +0300

    Switch to HomePage by default.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ export default class App extends React.Component {
     render() {
         return (
             <ErrorBoundary>
-                <FilmDetailsPage />
+                <HomePage />
                 {/* <FilmDetailsPage /> */}
             </ErrorBoundary>
         );
